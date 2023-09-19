78a9e3849003330c5fd54229d15d73039c831a57
MishBunny <mishelminecraft@gmail.com>
2023-09-19 17:41:04 +0300

    WOWWW

---
+++ app/src/main/java/il/co/mish/blogs/ACTIVITIES/BaseActivity.java
@@ -0,0 +1,16 @@
+package il.co.mish.blogs.ACTIVITIES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import il.co.mish.blogs.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
