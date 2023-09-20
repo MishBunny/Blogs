1aef0d35109c472f1b22a401a9c082797167eb00
MishBunny <mishelminecraft@gmail.com>
2023-09-20 19:34:30 +0300

    PROGRESS

--- MODEL/src/main/java/il/co/mish/model/BaseEntity.java
+++ MODEL/src/main/java/il/co/mish/model/BaseEntity.java
@@ -1,4 +1,21 @@
 package il.co.mish.model;
 
-public class BaseEntity {
+import androidx.annotation.Nullable;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable
+{
+    protected String idfs;
+    public BaseEntity()
+    {
+
+    }
+    public void SetIdfs(String idfs){ this.idfs = idfs; }
+    public String GetIdfs(){ return idfs; }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
--- MODEL/src/main/java/il/co/mish/model/BlogPost.java
+++ MODEL/src/main/java/il/co/mish/model/BlogPost.java
@@ -1,4 +1,42 @@
 package il.co.mish.model;
 
-public class BlogPost {
+import androidx.annotation.Nullable;
+
+import java.io.Serializable;
+
+public class BlogPost extends BaseEntity implements Serializable
+{
+    private String author;
+    private String title;
+    private String content;
+    private Long date;
+
+    public BlogPost(String author, String title, String content,Long date)
+    {
+        this.author=author;
+        this.title=title;
+        this.content=content;
+        this.date = date;
+    }
+
+
+    public String GetAuthor() { return author; }
+    public void SetAuthor(String author) { this.author=author; }
+
+    public String GetTitle() { return title; }
+    public void SetTitle(String title) { this.title=title; }
+
+
+    public String getContent() { return content; }
+
+    public void setContent(String content) { this.content = content; }
+
+    public Long getDate() { return date; }
+
+    public void setDate(Long date) { this.date = date; }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
